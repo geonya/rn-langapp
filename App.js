edb14125d65702228805b54b9b11172ce23b496c
geonya <geony8410@gmail.com>
2022-04-04 13:06:10 +0900

    ch4.7 more interpolation

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from "react";
-import { Animated, Easing, Pressable } from "react-native";
-import { TouchableOpacity } from "react-native";
+import { Animated, Pressable } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -8,7 +7,6 @@ const Container = styled.View`
 	justify-content: center;
 	align-items: center;
 `;
-// touchableOpacity 는 animation이 제대로 작동하지 않음. 그래서 View 에 animation 을 적용하고 TouchableOpacity로 감싸면 됨
 const Box = styled.View`
 	background-color: tomato;
 	width: 200px;
@@ -18,33 +16,35 @@ const AnimateBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
 	const [up, setUp] = useState(false);
-	// useRef 는 다시 렌더링이 일어나더라도 value를 유지하게 해주는 hook 이다.
-	// 쓰는 이유는 toggleUp 이 실행되면 state가 변경되어 전체 코드가 렌더링 되고 Y value 는 초기값 0으로 돌아가기 때문이다.
-	const Y_POSITION = useRef(new Animated.Value(300)).current;
+	const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 300 })).current;
 	const toggleUp = () => setUp((prev) => !prev);
 	const moveUp = () => {
-		Animated.timing(Y_POSITION, {
+		Animated.timing(POSITION, {
 			toValue: up ? 300 : -300,
-			useNativeDriver: true,
+			useNativeDriver: false,
 			duration: 2000,
 		}).start(toggleUp);
 	};
-	const opacity = Y_POSITION.interpolate({
-		inputRange: [-300, 0, 300],
-		outputRange: [1, 0.5, 1],
+	const rotateY = POSITION.y.interpolate({
+		inputRange: [-300, 300],
+		outputRange: ["-360deg", "360deg"],
 	});
-	const borderRadius = Y_POSITION.interpolate({
+	const borderRadius = POSITION.y.interpolate({
 		inputRange: [-300, 300],
 		outputRange: [100, 0],
+	});
+	const backgroundColor = POSITION.y.interpolate({
+		inputRange: [-300, 300],
+		outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
 	});
 	return (
 		<Container>
 			<Pressable onPress={moveUp}>
 				<AnimateBox
 					style={{
-						opacity,
 						borderRadius,
-						transform: [{ translateY: Y_POSITION }],
+						backgroundColor,
+						transform: [{ rotateY }, { translateY: POSITION.y }],
 					}}
 				/>
 			</Pressable>
