d4de78b594f966775d94840da5bdc9da4b983534
geonya <geony8410@gmail.com>
2022-04-04 13:34:08 +0900

    ch4.8 ValueXY

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { Animated, Pressable } from "react-native";
+import { Animated, Dimensions, Pressable } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -14,21 +14,48 @@ const Box = styled.View`
 `;
 const AnimateBox = Animated.createAnimatedComponent(Box);
 
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+
 export default function App() {
-	const [up, setUp] = useState(false);
-	const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 300 })).current;
-	const toggleUp = () => setUp((prev) => !prev);
-	const moveUp = () => {
-		Animated.timing(POSITION, {
-			toValue: up ? 300 : -300,
-			useNativeDriver: false,
-			duration: 2000,
-		}).start(toggleUp);
-	};
-	const rotateY = POSITION.y.interpolate({
-		inputRange: [-300, 300],
-		outputRange: ["-360deg", "360deg"],
+	const POSITION = useRef(
+		new Animated.ValueXY({
+			x: -SCREEN_WIDTH / 2 + 100,
+			y: -SCREEN_HEIGHT / 2 + 100,
+		})
+	).current;
+	const topLeft = Animated.timing(POSITION, {
+		toValue: {
+			x: -SCREEN_WIDTH / 2 + 100,
+			y: -SCREEN_HEIGHT / 2 + 100,
+		},
+		useNativeDriver: false,
 	});
+	const bottomLeft = Animated.timing(POSITION, {
+		toValue: {
+			x: -SCREEN_WIDTH / 2 + 100,
+			y: +SCREEN_HEIGHT / 2 - 100,
+		},
+		useNativeDriver: false,
+	});
+	const bottomRight = Animated.timing(POSITION, {
+		toValue: {
+			x: +SCREEN_WIDTH / 2 - 100,
+			y: +SCREEN_HEIGHT / 2 - 100,
+		},
+		useNativeDriver: false,
+	});
+	const topRight = Animated.timing(POSITION, {
+		toValue: {
+			x: +SCREEN_WIDTH / 2 - 100,
+			y: -SCREEN_HEIGHT / 2 + 100,
+		},
+		useNativeDriver: false,
+	});
+	const moveUp = () => {
+		Animated.loop(
+			Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
+		).start();
+	};
 	const borderRadius = POSITION.y.interpolate({
 		inputRange: [-300, 300],
 		outputRange: [100, 0],
@@ -37,6 +64,7 @@ export default function App() {
 		inputRange: [-300, 300],
 		outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
 	});
+	POSITION.addListener(() => console.log(POSITION.getTranslateTransform()));
 	return (
 		<Container>
 			<Pressable onPress={moveUp}>
@@ -44,7 +72,7 @@ export default function App() {
 					style={{
 						borderRadius,
 						backgroundColor,
-						transform: [{ rotateY }, { translateY: POSITION.y }],
+						transform: [...POSITION.getTranslateTransform()],
 					}}
 				/>
 			</Pressable>
