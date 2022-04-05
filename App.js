701d9396b7a918281b728a9b0433f7d0970d9144
geonya <geony8410@gmail.com>
2022-04-05 12:38:26 +0900

    #4.13 Card Project part Two

--- App.js
+++ App.js
@@ -21,37 +21,64 @@ const Card = styled(Animated.createAnimatedComponent(View))`
 `;
 
 export default function App() {
+	// Values
+	const scale = useRef(new Animated.Value(1)).current;
+	const position = useRef(new Animated.Value(0)).current;
+	const rotation = position.interpolate({
+		inputRange: [-250, 250],
+		outputRange: ["-15deg", "15deg"],
+		extrapolate: "clamp", //input이 범위 밖으로 벗어났을 때 clamp 옵션은 더이상 진행을 제한함
+	});
+	//Animations
+	const onPressIn = Animated.spring(scale, {
+		toValue: 0.95,
+		useNativeDriver: true,
+	});
+	const onPressOut = Animated.spring(scale, {
+		toValue: 1,
+		useNativeDriver: true,
+	});
+	const goCenter = Animated.spring(position, {
+		toValue: 0,
+		useNativeDriver: true,
+	});
+	// Pan Responders
 	const panResponder = useRef(
 		PanResponder.create({
 			onStartShouldSetPanResponder: () => true,
 			onPanResponderMove: (_, { dx }) => {
 				position.setValue(dx);
 			},
-			onPanResponderGrant: () => onPressIn(),
-			onPanResponderRelease: () => {
-				Animated.parallel([
-					onPressOut,
+			onPanResponderGrant: () => onPressIn.start(),
+			onPanResponderRelease: (_, { dx }) => {
+				if (dx < -300) {
 					Animated.spring(position, {
-						toValue: 0,
+						toValue: -400,
 						useNativeDriver: true,
-					}),
-				]).start();
+					}).start();
+				} else if (dx > 300) {
+					Animated.spring(position, {
+						toValue: 400,
+						useNativeDriver: true,
+					}).start();
+				} else {
+					Animated.parallel([onPressOut, goCenter]).start();
+				}
 			},
 		})
 	).current;
-	const scale = useRef(new Animated.Value(1)).current;
-	const position = useRef(new Animated.Value(0)).current;
-	const onPressIn = () =>
-		Animated.spring(scale, { toValue: 0.95, useNativeDriver: true }).start();
-	const onPressOut = Animated.spring(scale, {
-		toValue: 1,
-		useNativeDriver: true,
-	});
+
 	return (
 		<Container>
 			<Card
 				{...panResponder.panHandlers}
-				style={{ transform: [{ scale }, { translateX: position }] }}
+				style={{
+					transform: [
+						{ scale },
+						{ translateX: position },
+						{ rotateZ: rotation },
+					],
+				}}
 			>
 				<Ionicons name="pizza" color="#34495e" size={98} />
 			</Card>
